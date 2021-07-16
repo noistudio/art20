3baf563f06daf8a3446b59a98b49d5a556be1bbc
NoiStudio <artem@noibiz.com>
2021-07-16 14:46:22 +0400

    Update artadmin.php

--- application/config/artadmin.php
+++ application/config/artadmin.php
@@ -30,8 +30,9 @@ return [
 
     "sidebar_menu"=>[
         ["route_name"=>"artadmin.index","title"=>"Главная","icon"=>'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home feather-icon"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>'],
+        ["route_name"=>"artcrud.create",'permission'=>'manage_crud',"title"=>"Создать таблицу","icon"=>'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home feather-icon"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>'],
 
-
+    
 
 
 
