ad819ff7e435bbab78abac5fefc4663a48640090
NoiStudio <artem@noibiz.com>
2021-10-28 19:52:27 +0400

    Create api_error_sender.php

---
+++ application/config/api_error_sender.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    "is_enable"=>false,
+    'secretId'=>'secretToken',
+    'url'=>'https://yandex.ru/',
+    'name_task'=>'Ошибка [code]- [file]([line])',
+    'fields'=>[
+        "comment"=>"#беклог #error_[code]",
+        "type"=>2,
+        "sprint"=>4
+
+    ],
+
+
+];
