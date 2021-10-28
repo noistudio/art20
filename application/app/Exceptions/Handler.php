91c52370ddda5365d96deca677be59b5086c55e6
NoiStudio <artem@noibiz.com>
2021-10-28 19:57:38 +0400

    Update Handler.php

--- application/app/Exceptions/Handler.php
+++ application/app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use App\Models\ErrorLoger;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,6 +37,8 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+            $logger=new ErrorLoger();
+            $logger->send($e);;
         });
     }
 }
