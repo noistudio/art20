7f2942d90b121efa1329d2e544280afeffc855da
NoiStudio <artem@noibiz.com>
2021-11-08 21:37:57 +0400

    Update api_error_sender.php

--- application/config/api_error_sender.php
+++ application/config/api_error_sender.php
@@ -4,7 +4,7 @@ return [
     "is_enable"=>false,
     'secretId'=>'secretToken',
     'url'=>'https://yandex.ru/',
-    'name_task'=>'Ошибка [code]- [file]([line])',
+     'name_task'=>'Ошибка [code]- [file_name]([line])',
     'fields'=>[
         "comment"=>"#беклог #error_[code]",
         "type"=>2,
