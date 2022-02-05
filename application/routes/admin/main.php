66717a15bb9671837c8a207d3aca9e89fa5c7085
NoiStudio <artem@noibiz.com>
2022-02-05 12:22:22 +0400

    Update main.php

--- application/routes/admin/main.php
+++ application/routes/admin/main.php
@@ -3,4 +3,7 @@
 Route::group(['prefix' => 'filemanager'], function () {
     \UniSharp\LaravelFilemanager\Lfm::routes();
 });
-
+$config = array_merge(config('translation-manager.admin_route'), ['namespace' => 'Barryvdh\TranslationManager']);
+Route::group($config, function($router) {
+    $router->get('/', 'Controller@getIndex')->name('admin.trans.index');
+ });
