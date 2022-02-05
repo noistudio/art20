a7afd878aabec91142040f324ad3d5e09c87f5f4
NoiStudio <artem@noibiz.com>
2022-02-05 11:55:16 +0400

    Create setting_conf.php

---
+++ application/config/setting_conf.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+    'main'=>[
+         'description'=>'Главные настройки сайта',
+        'fields'=>[
+            'title'=>[
+                'title'=>'Название сайта',
+                'type'=>'text',
+            ],
+            'description'=>[
+                'title'=>'Описание сайта',
+                'type'=>'textarea',
+            ],
+            'keywords'=>[
+                'title'=>'Ключевые слова',
+                'type'=>'textarea',
+            ],
+          
+        ]
+
+
+    ],
+    
+);
