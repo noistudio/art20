84b72c62f8eddff416a6934580f652cf940ed6c8
NoiStudio <artem@noibiz.com>
2022-02-05 11:57:13 +0400

    Create laravellocalization.php.php

---
+++ application/config/laravellocalization.php.php
@@ -0,0 +1,23 @@
+<?php return array (
+  'supportedLocales' => 
+  array (
+    'en' => 
+    array (
+      'name' => 'English',
+      'script' => 'Latn',
+      'native' => 'English',
+    ),
+    'es' => 
+    array (
+      'name' => 'Spanish',
+      'script' => 'Latn',
+      'native' => 'español',
+    ),
+    'ru' => 
+    array (
+      'name' => 'Russian',
+      'script' => 'Cyrl',
+      'native' => 'русский',
+    ),
+  ),
+);
