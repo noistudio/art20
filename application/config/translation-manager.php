7bde9c376618550d9e6253f3a817a61bed4cbbbd
NoiStudio <artem@noibiz.com>
2022-02-05 11:55:58 +0400

    Create translation-manager.php

---
+++ application/config/translation-manager.php
@@ -0,0 +1,88 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routes group config
+    |--------------------------------------------------------------------------
+    |
+    | The default group settings for the elFinder routes.
+    |
+    */
+    'admin_route' => [
+        'prefix' => 'translations',
+
+        'middleware' => [
+            'web',
+            "artadmin.check_login",
+        ],
+    ],
+    'route' => [
+        'admin_prefix' => 'translations',
+        'prefix' => env('admin_prefix').'/translations',
+        'middleware' => [
+            'web',
+            "artadmin.check_login",
+        ],
+    ],
+
+    /**
+     * Enable deletion of translations
+     *
+     * @type boolean
+     */
+    'delete_enabled' => true,
+
+    /**
+     * Exclude specific groups from Laravel Translation Manager.
+     * This is useful if, for example, you want to avoid editing the official Laravel language files.
+     *
+     * @type array
+     *
+     *    array(
+     *        'pagination',
+     *        'reminders',
+     *        'validation',
+     *    )
+     */
+    'exclude_groups' => [
+        'auth',
+        'pagination',
+        'passwords',
+        'validation',
+        'vendor/admin_blocks',
+        'vendor/artadmin',
+    ],
+
+    /**
+     * Exclude specific languages from Laravel Translation Manager.
+     *
+     * @type array
+     *
+     *    array(
+     *        'fr',
+     *        'de',
+     *    )
+     */
+    'exclude_langs'  => [],
+
+    /**
+     * Export translations with keys output alphabetically.
+     */
+    'sort_keys'     => false,
+
+    'trans_functions' => [
+        'trans',
+        'trans_choice',
+        'Lang::get',
+        'Lang::choice',
+        'Lang::trans',
+        'Lang::transChoice',
+        '@lang',
+        '@choice',
+        '__',
+        '$trans.get',
+    ],
+
+];
