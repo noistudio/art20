a7f6f531c35f1feed7ed5ea7ba7c3abf65d54912
NoiStudio <artem@noibiz.com>
2022-02-05 11:51:42 +0400

    Update tables.php

--- application/routes/admin/tables.php
+++ application/routes/admin/tables.php
@@ -1,7 +1,11 @@
 <?php
 
 
-
+Route::get("/setting/index",[\App\Http\Controllers\admin\Setting::class,"index"])->name("admin.setting.index");
+Route::get("/setting/edit/{prefix}",[\App\Http\Controllers\admin\Setting::class,"edit"])->name("admin.setting.edit");
+Route::post("/setting/doupdate/{prefix}",[\App\Http\Controllers\admin\Setting::class,"doUpdate"])->name("admin.setting.doupdate");
+Route::get("/languages/index",[\App\Http\Controllers\admin\Languages::class,"form"])->name("admin.languages.form");
+Route::post("/languages/save",[\App\Http\Controllers\admin\Languages::class,"save"])->name("admin.languages.save");
 
 
 
